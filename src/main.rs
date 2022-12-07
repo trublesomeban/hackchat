80aefeec142629f9a92ca2a352dcf73ef61283a9
trublesomeban <70474429+trublesomeban@users.noreply.github.com>
2022-12-07 15:24:52 +0000

    Added chat

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
-#[macro_use] extern crate rocket;
-use std::time::SystemTime;
-// Try visiting:
-//   http://127.0.0.1:8000/wave/Rocketeer/100
+#[macro_use]
+extern crate rocket;
+
+use rocket::{form::Form, tokio::fs::read_to_string};
+use rocket::response::content::RawHtml;
+
+use std::{fs::OpenOptions, io::Write};
+
+#[derive(FromForm)]
+struct PostMsg {
+    content: String,
+}
+
+#[post("/chat", data = "<msg>")]
+async fn chat(msg: Form<PostMsg>) -> RawHtml<String> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("chat.txt")
+        .unwrap();
+    writeln!(file, "[anonymous]: {}<br>", msg.content).unwrap();
+
+    return RawHtml(format!(
+        "{}{}",
+        // msg.content,
+        read_to_string("chat.txt").await.unwrap(),
+        include_str!("../html/chat.html")
+    ));
+}
+
 #[get("/")]
-fn wave() -> String {
-    println!("{:?}", SystemTime::now());
-    format!("Witaj, błaźnie!\nTest wielu wierszy.")
+async fn index() -> RawHtml<String> {
+    return RawHtml(format!(
+        "{}{}",
+        // msg.content,
+        read_to_string("chat.txt").await.unwrap(),
+        include_str!("../html/chat.html")
+    ))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/wave", routes![wave])
+        .mount("/", routes![index])
+        .mount("/", routes![chat])
 }
