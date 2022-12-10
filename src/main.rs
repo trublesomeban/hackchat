8b049d273764959cd08a301a87c7e70ec6f08394
trublesomeban <70474429+trublesomeban@users.noreply.github.com>
2022-12-10 10:55:12 +0000

    additional changes

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,26 @@ async fn chat(username: String, msg: Form<PostMsg>) -> RawHtml<String> {
         .open("chat.txt")
         .unwrap();
     let _stamp = chrono::offset::Utc::now().to_string();
-    let timestamp = _stamp.split(".").last().unwrap();
+    let timestamp = _stamp.split(".").next().unwrap();
     if !msg.content.is_empty() {
         writeln!(
             file,
-            "<span class=\"{username}-username username\">[{username}]</span> <a class=\"msgbox\">{}</a> <span class=\"timestamp\">{timestamp}</span><br>",
+            "<span class=\"timestamp\">{timestamp}</span> <span class=\"{username}-username username\">[{username}]</span> <a class=\"{username}-msg msgbox\">{}</a><br>",
             msg.content
         )
         .unwrap();
     }
     return RawHtml(format!(
-        "<div class=\"chatbox\">{}{}</div>",
+        "<div class=\"chatbox\"><div class=\"title\">Hackchat.exe</div><br>{}{}</div>",
         // msg.content,
         read_to_string("chat.txt").await.unwrap(),
         read_to_string("html/chat.html").await.unwrap()
     ));
+}
+
+#[get("/chdata")]
+async fn chdata() -> String {
+    read_to_string("chat.txt").await.unwrap()
 }
 
 #[get("/login")]
@@ -49,4 +54,5 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![login])
         .mount("/", routes![chat])
+        .mount("/", routes![chdata])
 }
